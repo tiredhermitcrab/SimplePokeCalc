89d50e23d2bf93acb15aeddf8d69abdc69d65eb2
tiredhermitcrab <aidis975@gmail.com>
2023-01-06 00:51:18 +0900

    쇼다운 계산기 번들화

--- util/data/pokemon_en.js
+++ util/data/pokemon_en.js
@@ -1,4 +1,4 @@
-[
+export default [
 	"Bulbasaur",
 	"Ivysaur",
 	"Venusaur",
--- util/translate.js
+++ util/translate.js
@@ -1,32 +1,28 @@
-import * as fs from 'fs'
+import pokemon_en from './data/pokemon_en.js'
+import pokemon_ko from './data/pokemon_ko.js'
+import move_en from './data/move_en.js'
+import move_ko from './data/move_ko.js'
+import nature_en from './data/nature_en.js'
+import nature_ko from './data/nature_ko.js'
+import item_en from './data/item_en.js'
+import item_ko from './data/item_ko.js'
 
-const P = {
-    pokemon : name => {
-        const en = JSON.parse(fs.readFileSync('util/data/pokemon_en.json').toString())
-        const ko = JSON.parse(fs.readFileSync('util/data/pokemon_ko.json').toString())
-        
-        return en[ko.indexOf(name)] || ko[en.indexOf(name)]
-    },
+const T = {
+    pokemon : name => (
+        pokemon_en[pokemon_ko.indexOf(name)] || pokemon_ko[pokemon_en.indexOf(name)] || "Gengar"
+    ),
     move : name => {
-        const en = JSON.parse(fs.readFileSync('util/data/move_en.json').toString())
-        const ko = JSON.parse(fs.readFileSync('util/data/move_ko.json').toString())
-        
-        return en[ko.indexOf(name)] || ko[en.indexOf(name)]
+        return move_en[move_ko.indexOf(name)] || move_ko[move_en.indexOf(name)] || "Dark Pulse"
     },
     nature : name => {
-        const en = JSON.parse(fs.readFileSync('util/data/nature_en.json').toString())
-        const ko = JSON.parse(fs.readFileSync('util/data/nature_ko.json').toString())
-        
-        return en[ko.indexOf(name)] || ko[en.indexOf(name)]
+        return nature_en[nature_ko.indexOf(name)] || nature_ko[nature_en.indexOf(name)] || "Hardy"
     },
     item : name => {
-        const en = JSON.parse(fs.readFileSync('util/data/item_en.json').toString())
-        const ko = JSON.parse(fs.readFileSync('util/data/item_ko.json').toString())
-        
-        return en[ko.indexOf(name)] || ko[en.indexOf(name)]
+        return item_en[item_ko.indexOf(name)] || item_ko[item_en.indexOf(name)] || ""
     }
 };
 
 
+window.T = T;
 
-export default P;
+export default T;
